2e0921ab0e6ccb11df32c7bb6bae62d45f75c3fd
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-11-22 18:06:30 -0300

    feat: two-way binging , event binding, property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,24 @@ import { TitleComponent } from './header/title.component';
   standalone: true,
   imports: [CommonModule, RouterOutlet, TitleComponent],
   template: `<app-title
-      title="Mudou"
+      title="Anderson - Angular"
       name="Anderson"
       email="Gt1g4@example.com"
       *ngIf="aparecer"
     ></app-title>
-    <button (click)="destroy()">Destruir</button>
+    <button (click)="destroy()" [disabled]="!aparecer">Destruir</button>
+    {{ Destrui }}
+    <button (click)="aparecer = !aparecer" [disabled]="aparecer">Mostrar</button>
     <router-outlet></router-outlet>`,
 })
 export class AppComponent implements OnInit {
   public aparecer: boolean = true;
+  public Destrui: string = '';
 
   constructor() {}
   destroy(): void {
-    this.aparecer = !this.aparecer;
+    this.aparecer = false;
+    this.Destrui = 'Agora foi bem Destruido';
   }
   ngOnInit(): void {
     console.log('Anderson chegou!');

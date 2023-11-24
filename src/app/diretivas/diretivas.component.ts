a13f47a5784102f14262611c73948cd817efc7ea
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-11-23 23:51:49 -0300

    feat: componente para estudar as diretivas

---
+++ src/app/diretivas/diretivas.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-diretivas',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './diretivas.component.html',
+  styleUrl: './diretivas.component.css'
+})
+export class DiretivasComponent {
+
+}

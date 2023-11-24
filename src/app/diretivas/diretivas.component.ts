3c8a7b5224cea0b22346b267cbe567c1d5eff09c
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-11-24 18:13:28 -0300

    Estundando sobre o ngSwitch diretivas

--- src/app/diretivas/diretivas.component.ts
+++ src/app/diretivas/diretivas.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-diretivas',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './diretivas.component.html',
   styleUrl: './diretivas.component.css',
 })
 export class DiretivasComponent {
-
   public condicao = true;
-  public mostrar = true
+  public mostrar = true;
+  public name: string = '';
+  public placeholder: string = 'Digite seu nome';
 
-  public diretivasFor = [
+  public diretivasFor: Array<{
+    id: number;
+    name: string;
+    idade: number;
+    sex: string;
+  }> = [
     {
       id: 1,
       name: 'Kalleo',
@@ -38,11 +45,28 @@ export class DiretivasComponent {
     return {
       'classe-ativa': this.condicao,
       'classe-inativa': !this.condicao,
-      'classe-idade': this.mostrar
+      'classe-idade': this.mostrar,
     };
   }
 
   public onClickMostre() {
-    this.mostrar = !this.mostrar
+    this.mostrar = !this.mostrar;
+  }
+
+  public adicionar() {
+    this.diretivasFor.push({
+      id: this.diretivasFor.length + 1,
+      name: 'Bruno',
+      idade: 25,
+      sex: 'male',
+    });
+  }
+
+  public remover(event: number) {
+    this.diretivasFor.splice(event, 1);
+  }
+
+  public isName(name: string) {
+    return name.trim().toLowerCase();
   }
 }
